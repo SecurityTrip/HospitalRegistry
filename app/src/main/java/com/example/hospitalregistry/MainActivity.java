31564c4266f7f72f1c8ee4d0d9af2e2b97f41aa5
Ilia <illigiorno@gmail.com>
2024-03-22 22:43:35 +0400

    Initial commit

---
+++ app/src/main/java/com/example/hospitalregistry/MainActivity.java
@@ -0,0 +1,60 @@
+package com.example.hospitalregistry;
+
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.hospitalregistry.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        com.example.hospitalregistry.databinding.ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        replaceFragment(new QueueFragment());
+
+        binding.bottomNavigationView.setOnNavigationItemSelectedListener(menuItem -> {
+
+            int itemId = menuItem.getItemId();
+
+            if (itemId == R.id.queue){
+                replaceFragment(new QueueFragment());
+            } else if(itemId == R.id.research){
+                replaceFragment(new ResearchFragment());
+            }else if (itemId == R.id.archive){
+                replaceFragment(new ArchiveFragment());
+            } else if (itemId == R.id.profile) {
+                replaceFragment(new PersonFragment());
+            }
+
+            return true;
+        });
+
+
+    }
+
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
+    }
+
+}
